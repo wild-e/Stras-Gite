7ffb297e9d3bec30cac0e1f030e35d421093b06c
wild-e <62098185+wild-e@users.noreply.github.com>
2020-11-05 10:27:05 +0100

    Update UserManager.php
    test

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -18,7 +18,8 @@ class UserManager extends AbstractManager
 
     public function login(array $login)
     {
-        $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE email = (:email) AND password= (:password)");
+        $statement = $this->pdo->prepare
+            ("SELECT * FROM " . self::TABLE . " WHERE email = (:email) AND password= (:password)");
         $statement->bindValue(':email', $login['email'], \PDO::PARAM_STR);
         $statement->bindValue(':password', $login['password'], \PDO::PARAM_STR);
         $statement->execute();
