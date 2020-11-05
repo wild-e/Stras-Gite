1e99634d81427a6f6bb257f2b9deadc41743835e
wild-e <62098185+wild-e@users.noreply.github.com>
2020-11-05 10:04:26 +0100

    Update UserManager.php
    fixed errors to pass build

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -10,18 +10,18 @@ class UserManager extends AbstractManager
 {
     const TABLE = 'clients';
 
-    // initializes this class 
+    // initializes this class
     public function __construct()
     {
         parent::__construct(self::TABLE);
     }
 
-    public function login(array $login){
+    public function login(array $login)
+    {
         $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE email = (:email) AND password= (:password)");
         $statement->bindValue(':email', $login['email'], \PDO::PARAM_STR);
         $statement->bindValue(':password', $login['password'], \PDO::PARAM_STR);
         $statement->execute();
         return $statement->fetch();
     }
-
 }
