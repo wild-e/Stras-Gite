ecb02a669c5aa65ae0d85aa093c71dd9db22971f
wild-e <wild.emmanuelle@gmail.com>
2020-11-10 10:12:26 +0100

    used ./vendor/bin/phpcs before pushing

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,14 +7,14 @@ class AdminController extends AbstractController
     public function showBooking()
     {
         if ($_SESSION['is_admin'] == 1) {
-        return $this->twig->render('Admin/booking.html.twig');
+            return $this->twig->render('Admin/booking.html.twig');
         }
     }
 
     public function messageShow()
     {
         if ($_SESSION['is_admin'] == 1) {
-        return $this->twig->render('Admin/messageShow.html.twig');
+            return $this->twig->render('Admin/messageShow.html.twig');
         }
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,14 +7,14 @@ class UserController extends AbstractController
     public function showBooking()
     {
         if ($_SESSION['is_admin'] == 0) {
-        return $this->twig->render('User/booking.html.twig');
+            return $this->twig->render('User/booking.html.twig');
         }
     }
 
     public function writeReview()
     {
         if ($_SESSION['is_admin'] == 0) {
-        return $this->twig->render('User/writeReview.html.twig');
+            return $this->twig->render('User/writeReview.html.twig');
         }
     }
 }
